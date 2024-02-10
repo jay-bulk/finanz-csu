2852d848643f2ea486ba2a8e6460d003231d4573
jay-bulk <jay.bulk@tuta.io>
2024-02-09 22:51:37 -0700

    feat: first iteration schema

--- schema.sql
+++ schema.sql
@@ -1,1 +1,32 @@
-create table budget ()
+
+CREATE TABLE IF NOT EXISTS users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS category (
+  category_id INT AUTO_INCREMENT PRIMARY KEY,
+  category_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS user_budget (
+    budget_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    budget_amount FLOAT(7,2),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS expense_income ();
+
+
+CREATE TABLE IF NOT EXISTS budget_line (
+  expense_line INT AUTO_INCREMENT PRIMARY_KEY,
+  category_id INT,
+  year_id INT,
+  month_id INT,
+  expense_total FLOAT(7,2),
+  FOREIGN KEY (category_id) REFERENCES category(category_id)
+);
