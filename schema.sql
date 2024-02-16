4853f4071592d094c82f5f63a47eaa91ea651216
jay-bulk <jay.bulk@tuta.io>
2024-02-15 23:10:29 -0700

    fix: update sql schema generation

--- schema.sql
+++ schema.sql
@@ -1,32 +1,42 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS user_budget;
+DROP TABLE IF EXISTS expense_income;
+DROP TABLE IF EXISTS budget_line;
 
-CREATE TABLE IF NOT EXISTS users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE users (
+    user_id INT IDENTITY(1,1) PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
     password VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS category (
-  category_id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE category (
+  category_id INT IDENTITY(1,1) PRIMARY KEY,
   category_name VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS user_budget (
-    budget_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    budget_amount FLOAT(7,2),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+CREATE TABLE user_budget (
+  budget_id INT IDENTITY(1,1) PRIMARY KEY,
+  user_id INT,
+  budget_amount DECIMAL(10,2),
+  month_difference DECIMAL(10,2),
+  created_at TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS expense_income ();
-
-
-CREATE TABLE IF NOT EXISTS budget_line (
-  expense_line INT AUTO_INCREMENT PRIMARY_KEY,
+CREATE TABLE budget_line (
+  expense_line INT IDENTITY(1,1) PRIMARY KEY,
   category_id INT,
   year_id INT,
   month_id INT,
-  expense_total FLOAT(7,2),
-  FOREIGN KEY (category_id) REFERENCES category(category_id)
+  expense_total DECIMAL(10,2)
 );
+
+CREATE TABLE expense_income (
+  charge_id INT IDENTITY(1,1) PRIMARY KEY,
+  user_id INT,
+  month_id INT,
+  income_amount DECIMAL(10,2),
+  expense_amount DECIMAL(10,2),
+);
+
