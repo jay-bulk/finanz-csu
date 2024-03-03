98121dce525f3d4253c9968c8f24c02c8ee289f7
jay-bulk <jay.bulk@tuta.io>
2024-03-03 00:00:36 -0700

    fix: register vs login

--- Malone-Bulkley-CSU-dotNET/Controllers/Account/Login.cs
+++ Malone-Bulkley-CSU-dotNET/Controllers/Account/Login.cs
@@ -41,4 +41,15 @@ public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
             ModelState.AddModelError("", "Invalid login attempt.");
             return View(model);
     }
+
+    private async Task<string> SendEmailConfirmationTokenAsync(string userID, string subject)
+    {
+        string code = await UserManager.GenerateEmailConfirmationTokenAsync(userID);
+        var callbackUrl = Url.Action("ConfirmEmail", "Account",
+           new { userId = userID, code = code }, protocol: Request.Url.Scheme);
+        await UserManager.SendEmailAsync(userID, subject,
+           "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+        return callbackUrl;
+    }
 }
